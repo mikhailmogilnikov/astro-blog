17643a787531f5b25ed1d4b7aaadf4c0779d0674
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2025-03-22 21:24:34 +0300

    feat: tags pages, rss feed, styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const { PUBLIC_BASE_URL } = loadEnv(
+  process.env.NODE_ENV || "production",
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: PUBLIC_BASE_URL,
+});
